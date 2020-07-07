6ae014517485352cf11ffd54b429d72a65627be2
jonghoon <jonghoon70@gmail.com>
2020-07-07 17:24:44 +0900

    add postMemo

--- src/controllers/v1/bchain.controller.js
+++ src/controllers/v1/bchain.controller.js
@@ -1,21 +1,45 @@
+require('dotenv').config();
+
 var debug = require('debug')('restapi:server');
+import net from 'net'
 import Web3 from 'web3';
 
-const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
-
+const web3 = new Web3(new Web3.providers.IpcProvider(process.env.GETH_IPC, net));
 const getBlockNumber = async (req, res, next) => {
   try {
-    let result = {};
+    let result = {};    
     const blockNumber = await web3.eth.getBlockNumber();
     result.blockNumber = blockNumber;
     res.json(result);
   } catch (err) {
-    // if (conn) conn.release();
     throw err;
-  } finally {
+  } finally {}
+}
+
+const postMemo = async (req, res, next) => {
+  try {
+    let result = {};
+    const coinbase = await web3.eth.getCoinbase();
+    const isUnlock = await web3.eth.personal.unlockAccount(coinbase, '1243', 1);
+    const txid = await web3.eth.sendTransaction({
+      from: coinbase,
+      to: '0x0000000000000000000000000000000000000000',
+      value: 0,
+      data: '31323334353637'
+    }).on('transactionHash', transactionhash => {
+      debug(new Date() + '4')
+      result.txid = transactionhash;
+      res.json(result);
+    }).on('error', error => {
+      result.errMsg = error.message;
+      res.json(result);
+    })
+  } catch (e) {
+    throw e;
   }
 }
 
 export {
-  getBlockNumber
+  getBlockNumber,
+  postMemo
 }
--- src/routes/v1/bchain.route.js
+++ src/routes/v1/bchain.route.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import { get } from '../../controllers/v1/bchain.controller'
+import { getBlockNumber, postMemo } from '../../controllers/v1/bchain.controller'
 
 const router = express.Router()
 
 router.route('/blockNumber').get((req, res, next) => {
-  get(req, res, next);
+  getBlockNumber(req, res, next);
+});
+
+router.route('/memo').post((req, res, next) => {
+  postMemo(req, res, next);
 });
 
 export default router
